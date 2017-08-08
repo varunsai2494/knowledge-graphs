c9da174a9abe604f547acc55e6f681112664d798
root <varunsai2494@gmail.com>
2017-08-08 21:09:38 +0530

    changes

--- knowledgegraphs/static/app.js
+++ knowledgegraphs/static/app.js
@@ -20,7 +20,7 @@ KnowledgeGraph.config(function($stateProvider, $urlRouterProvider) {
 });
 
 KnowledgeGraph.controller('HomeCtrl', function($scope, $http, $q){
-
+    $scope.saveStatus = false;
     $scope.sampleText = "Hello";
     $scope.sampleQuestion = {id: 0, text: 'Who is the Director of Interstellar?'};
     $scope.sampleAnswer = {id: 0, text: 'Christopher nolan directed this movie'};
@@ -34,21 +34,10 @@ KnowledgeGraph.controller('HomeCtrl', function($scope, $http, $q){
         {id: 1, text:"Heath Ledger's Acting got 10 likes,3 dislikes and 2 neutral responses"}
     ];
 
-    $scope.getSampleAnswer = function(question){
-        console.log(question);
-
-        for(var i=0; i<$scope.sampleAnswers.length; i++){
-            var answer = (question.id == $scope.sampleAnswers[i].id) ? ($scope.sampleAnswers[i]) : '';
-        }
-
-        $scope.sampleAnswer = answer;
-    };
-
     $scope.getAnswer = function(question){
-        console.log(JSON.stringify(question));
         $http({
             method: 'POST',
-            url: 'http://35.164.231.42:8000/qna',
+            url: 'http://localhost:8000/qna',
             headers: {
                 'content-type'  : 'application/json'
             },
@@ -58,7 +47,7 @@ KnowledgeGraph.controller('HomeCtrl', function($scope, $http, $q){
         })
         .then(function(response) {
             console.log(response);
-            $scope.userAnswer = response.text;
+            $scope.userAnswer = response.data.text;
         }, function(error) {
             console.log("Submit works");
             console.log("Error in returning answer from database", error);
@@ -66,10 +55,9 @@ KnowledgeGraph.controller('HomeCtrl', function($scope, $http, $q){
     };
 
     $scope.updateKnowledge = function(text){
-        console.log(text);
         $http({
             method: 'POST',
-            url: 'http://35.164.231.42:8000/updategraph',
+            url: 'http://localhost:8000/updategraph',
             headers: {
                 'content-type'  : 'application/json'
             },
@@ -78,8 +66,8 @@ KnowledgeGraph.controller('HomeCtrl', function($scope, $http, $q){
             }
         }).then(function(response) {
             console.log(response);
+            $scope.saveStatus = (response.data.text == "success") ? true : false;
         }, function(error) {
-            console.log("Submit works");
             console.log("Error in returning answer from database", error);
         });
     };
